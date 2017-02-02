110d70e7819c20a4eec3c61eb91869ab6b7e6c46
daedalus-solutions <jono@daedalus.co.za>
2017-02-02 11:19:23 +0200

    Update run.sh

--- sni-proxy/run.sh
+++ sni-proxy/run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -ex
 
 cp -f /etc/nginx/nginx.tmpl /etc/nginx/nginx.conf
-sed -i -e "s/APIM/${APIM_DNS}/g" /etc/nginx/nginx.conf
-sed -i -e "s/PORTAL/${PORTAL_DNS}/g" /etc/nginx/nginx.conf
-sed -i -e "s/GATEWAY/${GATEWAY_DNS}/g" /etc/nginx/nginx.conf
+sed -i -e "s/APIM_DNS/${APIM_DNS}/g" /etc/nginx/nginx.conf
+sed -i -e "s/PORTAL_DNS/${PORTAL_DNS}/g" /etc/nginx/nginx.conf
+sed -i -e "s/GATEWAY_DNS/${GATEWAY_DNS}/g" /etc/nginx/nginx.conf
+sed -i -e "s/APIM_URI/${APIM_URI}/g" /etc/nginx/nginx.conf
+sed -i -e "s/PORTAL_URI/${PORTAL_URI}/g" /etc/nginx/nginx.conf
+sed -i -e "s/GATEWAY_URI/${GATEWAY_URI}/g" /etc/nginx/nginx.conf
+sed -i -e "s/GWADMIN_URI/${GWADMIN_URI}/g" /etc/nginx/nginx.conf
 cat /etc/nginx/nginx.conf
 exec nginx -g "daemon off;"
